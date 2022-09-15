71d4e2ab09818d97531148531885ee549b3c4b03
Felipe Melo <felipe.leandro2@gmail.com>
2022-09-15 20:38:57 -0300

    Adicionando Spring Boot

---
+++ src/main/java/com/trugdz/santanderletscode/SantanderletscodeApplication.java
@@ -0,0 +1,13 @@
+package com.trugdz.santanderletscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderletscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderletscodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trugdz/santanderletscode/SantanderletscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trugdz.santanderletscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderletscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
